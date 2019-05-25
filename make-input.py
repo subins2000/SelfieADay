b276311cdfedee56d0e1946b26f926cc88534437
Subin Siby <subins2000@gmail.com>
2019-05-25 19:53:01 +0530

    Use library to resize images and fit to certain canvas

--- make-input.py
+++ make-input.py
@@ -1,7 +1,9 @@
+import math
 import os
 import glob
 from PIL import Image
 from PIL.ExifTags import TAGS
+from resizeimage import resizeimage
 import time
 
 
@@ -14,22 +16,12 @@ def get_exif(fn):
         ret[decoded] = value
     return ret
 
-
-def make_square(im, min_size=1000, fill_color=(0, 0, 0, 0)):
-    x, y = im.size
-    min_size = x if x > y else y
-    size = max(min_size, x, y)
-    new_im = Image.new('RGB', (size, size), fill_color)
-    new_im.paste(im, ((size - x) // 2, (size - y) // 2))
-    return new_im
-
-
 os.chdir('input')
 
 files = glob.glob('*.jpg')
 
+count = 0
 for file in files:
-    print(file)
     time = get_exif(file)['DateTimeOriginal']
 
     time = time.replace(':', '')
@@ -42,6 +34,9 @@ for file in files:
 
     im = Image.open(new_name)
     exif = im.info['exif']
-    
-    imResize = make_square(im)
-    imResize.save(new_name, 'JPEG', quality=100, exif=exif)
+
+    cover = resizeimage.resize_contain(im, [2500, 2500], bg_color=(0,0,0,1))
+    cover.save(new_name, im.format, quality=100, exif=exif)
+
+    print(str(count) + ' - ' + file)
+    count += 1
